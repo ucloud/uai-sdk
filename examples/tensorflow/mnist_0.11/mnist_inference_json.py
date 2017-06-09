39f5279c4a8f2b6c399a78c8b3a7f3d464eca5b0
root <charlie.song@ucloud.cn>
2017-06-09 17:27:27 +0800

    Update tf-0.11 mnist json example
    Signed-off-by Xiang Song<charlie.song@ucloud.cn>

--- examples/tensorflow/mnist_0.11/mnist_inference_json.py
+++ examples/tensorflow/mnist_0.11/mnist_inference_json.py
@@ -1,23 +1,18 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+# Copyright 2017 The UAI-SDK Authors. All Rights Reserved. 
+#                                                                                                                     
+# Licensed under the Apache License, Version 2.0 (the "License");                                                     
+# you may not use this file except in compliance with the License.                                                    
+# You may obtain a copy of the License at                                                                             
+#                                                                                                                     
+#     http://www.apache.org/licenses/LICENSE-2.0                                                                      
+#                                                                                                                     
+# Unless required by applicable law or agreed to in writing, software                                                 
+# distributed under the License is distributed on an "AS IS" BASIS,                                                   
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                                            
+# See the License for the specific language governing permissions and                                                 
+# limitations under the License.                                                                                      
+# ==============================================================================   
 
-"""A very simple MNIST classifier.
-
-See extensive documentation at
-http://tensorflow.org/tutorials/mnist/beginners/index.md
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -29,7 +24,7 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 
-from uai.ai_ucloud_tf import TFAiUcloudModel
+from uai.arch.tf_model import TFAiUcloudModel
 
 class MnistModel(TFAiUcloudModel):
   def __init__(self, conf):
