f9d22394651aefac353900968672c6b3fec89510
FinchZHU <30855713+FinchZHU@users.noreply.github.com>
2018-07-11 14:16:01 +0800

    Update retrain_conf.py

--- examples/tensorflow/inference/retrain/code/retrain_conf.py
+++ examples/tensorflow/inference/retrain/code/retrain_conf.py
@@ -6,15 +6,11 @@ class RetrainJsonConfLoader(TFJsonConfLoader):
 
     def _load(self):
         super(RetrainJsonConfLoader, self)._load()
-        self.model_struct = self.server_conf['tensorflow']['model_struct']
 	self.input_width = eval(self.server_conf['tensorflow']['input_width'])
 	self.input_height = eval(self.server_conf['tensorflow']['input_height'])
     
     def get_model_dir(self):
         return self.model_dir
-
-    def get_model_struct(self):
-        return self.model_struct
 
     def get_input_width(self):
     	return self.input_width
